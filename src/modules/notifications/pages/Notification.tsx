822cec2cc96f057e8f47736b5fd58294b8d8ddb7
shrutigxuriti <shruti.ghosh@xuriti.com>
2023-11-28 17:20:58 +0530

    notifications module completed

--- src/modules/notifications/pages/Notification.tsx
+++ src/modules/notifications/pages/Notification.tsx
@@ -1,23 +1,87 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import colors from '../../../../colors';
+import { responsiveFontSize, responsiveWidth } from 'react-native-responsive-dimensions';
+import { getNotifications } from '../../../utils/apiservice';
+
+interface NotificationItem {
+  alertDesc: string;
+  alertDate: string;
+}
 
 const Notification: React.FC = () => {
+  const [notifications, setNotifications] = useState<NotificationItem[]>([]);
+
+  useEffect(() => {
+    getNotifications().then(async (response) => {
+      console.log(response);
+      const result = await response.json();
+      console.log(result);
+      setNotifications(result);
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, This is a Plain Page!</Text>
-    </View>
+    <ScrollView style={styles.mainWrapper}>
+      {notifications.map((item, index) => (
+        <View key={index} style={styles.messageItem}>
+          <Image
+            style={styles.image}
+            source={require('../../../assets/images/ic_alert_.png')}
+          />
+          <View style={styles.messageContainer}>
+            <Text style={styles.messageHeader}>{item.alertDate}</Text>
+            <ScrollView style={styles.messageTextContainer} horizontal={true}>
+              <Text style={styles.messageText}>{item.alertDesc}</Text>
+            </ScrollView>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  mainWrapper: {
+    backgroundColor: colors.white,
     flex: 1,
-    justifyContent: 'center',
+    padding: 15,
+  },
+  header: {
+    color: colors.black,
+    fontWeight: 'bold',
+    fontSize: responsiveFontSize(2.5),
+    textAlign: 'center',
+  },
+  messageItem: {
+    borderBottomWidth: 1,
+    borderColor: colors.lightGrey,
+    paddingVertical: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20,
     alignItems: 'center',
   },
-  text: {
-    fontSize: 20,
+  messageHeader: {
+    fontSize: responsiveFontSize(1.5),
     fontWeight: 'bold',
+    color: colors.black,
+  },
+  messageText: {
+    fontSize: responsiveFontSize(1.7),
+    color: colors.black,
+  },
+  messageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  image: {
+    height: responsiveFontSize(5),
+    width: responsiveFontSize(5),
+  },
+  messageTextContainer: {
+    maxWidth: responsiveWidth(75),
+    overflow: 'hidden',
   },
 });
 
