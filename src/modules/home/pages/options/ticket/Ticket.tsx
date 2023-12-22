f74a0cb67d2002d48e17767bc75146e53e0c61ef
shrutigxuriti <shruti.ghosh@xuriti.com>
2023-12-22 12:46:12 +0530

    ticket history fix

--- src/modules/home/pages/options/ticket/Ticket.tsx
+++ src/modules/home/pages/options/ticket/Ticket.tsx
@@ -589,7 +589,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    alignSelf: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.7)',
   },
   modalContent: {
