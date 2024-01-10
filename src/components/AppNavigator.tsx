64d2fb42d4a6799f3a92bb85fda3a90a4c569aaa
shrutigxuriti <shruti.ghosh@xuriti.com>
2024-01-10 16:49:47 +0530

    welcome popup message

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -4,13 +4,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import BottomTab from '../modules/common/services/BottomTab';
 import AuthNavigator from '../modules/auth/stack/AuthNavigator';
 import { useAuth } from './AuthContext';
+import OpenPopupOnOpeningApp from './OpenPopupOnOpeningApp'; // Import the OpenPopupOnOpeningApp component
 
 const AppNavigator: React.FC = () => {
   const { isUserAuthenticated } = useAuth();
 
   return (
     <NavigationContainer>
-      {isUserAuthenticated ? <BottomTab /> : <AuthNavigator />}
+      {/* {isUserAuthenticated ? <BottomTab /> : <AuthNavigator />} */}
+      {isUserAuthenticated ? (
+        <>
+          <BottomTab />
+          <OpenPopupOnOpeningApp />
+        </>
+      ) : (
+        <AuthNavigator />
+      )}
     </NavigationContainer>
   );
 };
