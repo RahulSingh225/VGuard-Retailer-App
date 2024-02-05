603932ee402ab7c3e569293a122b989651963655
shrutigxuriti <shruti.ghosh@xuriti.com>
2024-02-05 15:33:11 +0530

    splash screen fix

--- src/modules/auth/pages/SplashScreen.tsx
+++ src/modules/auth/pages/SplashScreen.tsx
@@ -1,23 +1,18 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, StyleSheet, Image, Modal, TouchableOpacity } from 'react-native';
-import Buttons from '../../../components/Buttons';
-import colors from '../../../../colors';
-import language from '../../../assets/images/language.png';
-import arrowIcon from '../../../assets/images/arrow.png';
-import { useTranslation } from 'react-i18next';
-import LanguagePicker from '../../../components/LanguagePicker';
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
+import colors from '../../../../colors';
 
 const SplashScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
-  const { t, i18n } = useTranslation();
+  useFocusEffect(
+    React.useCallback(() => {
+      const timeoutId = setTimeout(() => {
+        navigation.navigate('login');
+      }, 1000);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      navigation.navigate('login');
-    }, 1000);
-
-    return () => clearTimeout(timeoutId);
-  }, [i18n.language]);
+      return () => clearTimeout(timeoutId);
+    }, [navigation])
+  );
 
   return (
     <View style={styles.mainWrapper}>
@@ -31,18 +26,6 @@ const SplashScreen: React.FC<{ navigation: any }> = ({ navigation }) => {
           style={styles.imageSaathi}
         />
       </View>
-      {/* <View style={styles.startButtonContainer}>
-        <Buttons
-          label={t('strings:start')}
-          variant={'filled'}
-          onPress={() => navigation.navigate('login')}
-          iconHeight={10}
-          iconWidth={30}
-          iconGap={30}
-          icon={arrowIcon}
-          width="90%"
-        />
-      </View> */}
     </View>
   );
 };
@@ -52,15 +35,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 25,
     backgroundColor: colors.white,
-  },
-  buttonContainer: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-end',
-  },
-  button: {
-    alignSelf: 'right',
   },
   imageContainer: {
     display: 'flex',
@@ -78,27 +52,6 @@ const styles = StyleSheet.create({
   imageSaathi: {
     width: 200,
     height: 196,
-  },
-  startButtonContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    height: '20%',
-  },
-  languagePickerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.white,
-  },
-  closeText: {
-    marginTop: 20,
-    color: colors.black,
-    backgroundColor: colors.yellow,
-    paddingHorizontal: 15,
-    paddingVertical: 5,
-    borderRadius: 5,
-    fontWeight: 'bold',
   },
 });
 
